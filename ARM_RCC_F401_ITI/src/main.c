321f36a882892fe76b26fcfce216bccfb349b245
Mahmoud Hussein Goda <mhmoudhgh@gmail.com>
2023-01-15 11:40:16 +0200

    Added the configurations and tried a few modification on the main

--- ARM_RCC_F401_ITI/src/main.c
+++ ARM_RCC_F401_ITI/src/main.c
@@ -10,7 +10,10 @@ int main()
 	MRCC_voidInit();
 	MRCC_voidPeripheralEnable(AHB1,GPIOAEN);
 	MGPIO_SetPinDirection(GPIO_PORTA,GPIO_Pin0,OUTPUT_PUSH_PULL,2);
+	MGPIO_SetPinDirection(GPIO_PORTA,GPIO_Pin1,OUTPUT_PUSH_PULL,2);
 	MCAL_voidSetPinValue(GPIO_PORTA,GPIO_Pin0,1);
+	MCAL_voidSetPinValue(GPIO_PORTA,GPIO_Pin1,1);
+
 	while(1)
 	{
 
